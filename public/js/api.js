6042338e3fac4b404fb8a32951d4df7e9e0337d3
seu-usersaneme <seu-email>
2022-12-15 20:16:13 -0300

    First commit

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+const api_key = "d850e255bd1456e2d68b2baab3a3e925"
+let img_url = "https://image.tmdb.org/t/p/w500/"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http ="https://api.themoviedb.org/3/discover/movie?"
